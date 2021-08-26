d00664bbb7312c8419f6f40c1b0468ea14b976a6
ibsenriou <ibsenriou@hotmail.com>
2021-08-26 11:01:11 -0300

    Curso concluído - Forumlários no Django3

--- passagens/forms.py
+++ passagens/forms.py
@@ -3,22 +3,21 @@ from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classes
 from passagens.validation import *
+from passagens.models import Passagem, ClasseViagem, Pessoa
 
 
-class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Volta', widget=DatePicker())
+class PassagemForms(forms.ModelForm):
     data_pesquisa = forms.DateField(label='Data da Pesquisa', disabled=True, initial=datetime.today())
-    classe_viagem = forms.ChoiceField(label='Classe do Vôo', choices=tipos_de_classes)
-    informacoes = forms.CharField(
-        label='Informações Extras',
-        max_length=200,
-        widget=forms.Textarea(),
-        required=False,
-    )
-    email = forms.EmailField(label='Email', max_length=150)
+
+    class Meta:
+        model = Passagem
+        fields = '__all__'
+        labels = {'data_ida': 'Data de ida', 'data_volta': 'Data de volta', 'informacoes': 'Informações',
+                  'classe_viagem': 'Classe do Voo'}
+        widgets = {
+            'data_ida': DatePicker(),
+            'data_volta': DatePicker()
+        }
 
     def clean(self):
         origem = self.cleaned_data.get('origem')
@@ -37,4 +36,10 @@ class PassagemForms(forms.Form):
                 mensagem_erro = lista_de_erros[erro]
                 self.add_error(erro, mensagem_erro)
         return self.cleaned_data
+
+
+class PessoaForms(forms.ModelForm):
+    class Meta:
+        model = Pessoa
+        exclude = ['nome']
 
--- passagens/views.py
+++ passagens/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-from passagens.forms import PassagemForms
+from passagens.forms import PassagemForms, PessoaForms
 
 
 def index(request):
     form = PassagemForms()
-    context = {'form': form}
+    pessoa_form = PessoaForms()
+    context = {'form': form, 'pessoa_form': pessoa_form}
     return render(request, 'index.html', context)
 
 
 def revisao_consulta(request):
     if request.method == 'POST':
         form = PassagemForms(request.POST)
+        pessoa_form = PessoaForms(request.POST)
         if form.is_valid():
-            context = {'form': form}
+            context = {'form': form, 'pessoa_form': pessoa_form}
             return render(request, 'minha_consulta.html', context)
+
         else:
             print('Form inválido')
-            context = {'form': form}
+            context = {'form': form, 'pessoa_form': pessoa_form}
             return render(request, 'index.html', context)
